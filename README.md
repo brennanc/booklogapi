63cea7ad5b0f2a4e3986023456412402a38ae600
Brennan Cropper <bcropper@houseloan.com>
2021-12-09 23:35:19 -0600

    added function app support

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # booklogapi
+
+`npm install @azure/data-tables`
+[NPM @azure/data-tables documentation](https://www.npmjs.com/package/@azure/data-tables)
+
+
+`npm install @azure/identity`
+
+[Tutorial: Create a function app that connects to Azure services using identities instead of secrets](https://docs.microsoft.com/en-us/azure/azure-functions/functions-identity-based-connections-tutorial)
--- bookApi/index.js
+++ bookApi/index.js
@@ -1,13 +1,63 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+const { TableServiceClient } = require("@azure/data-tables");
+const { DefaultAzureCredential } = require("@azure/identity");
 
+// DefaultAzureCredential expects the following three environment variables:
+// - AZURE_TENANT_ID: The tenant ID in Azure Active Directory
+// - AZURE_CLIENT_ID: The application (client) ID registered in the AAD tenant
+// - AZURE_CLIENT_SECRET: The client secret for the registered application
+const credential = new DefaultAzureCredential();
+const account = "booklogsa";
+
+const clientWithAAD = new TableServiceClient(
+  `https://${account}.table.core.windows.net`,
+  credential
+);
+
+const tableName = "booklog";
+
+
+module.exports = async function (context, req) {
+
+    context.res = undefined;
+
+    const method = req.method;
+    const id = req.params.id;
     const name = (req.query.name || (req.body && req.body.name));
+
+    let tablesIter = clientWithAAD.listTables();
+    for await (const table of tablesIter) {
+        console.log(`Table${i}: ${table.name}`);
+        i++;
+    }
+
+    if (method === "GET") {
+        context.log('Processed GET request');
+        if (id) {
+            context.log('with id ' + id);
+        } else {
+            context.log('for all');
+        }
+    } else if (method === "POST") {
+        context.log('Processed POST request');
+        if (id) {
+            context.res = {
+                status: 400
+            };
+        } else {
+            context.log('for all');
+        }
+    }
+
+
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
+    if (context.res == undefined) {
+        context.log('setting default response');
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: responseMessage
+        };
+    }
+};
